6867028ba31c0174f9d98c339cf5ab0f08297609
Juan Manuel Barea Martinez <jmbarea@atsistemas.com>
2020-11-08 18:06:08 +0100

    Disables context load test

---
+++ src/test/kotlin/org/jmb/azureeventhubproducer/AzureEventhubProducerApplicationTests.kt
@@ -0,0 +1,12 @@
+package org.jmb.azureeventhubproducer
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AzureEventhubProducerApplicationTests {
+
+    //@Test
+    fun contextLoads() {
+    }
+
+}
